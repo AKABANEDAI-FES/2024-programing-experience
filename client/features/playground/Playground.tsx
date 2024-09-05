e4acf17181d686083c90e4af975cca3fee009b81
yossuli <s1f102302348@iniad.org>
2024-09-05 09:30:34 +0900

    chore: delete sample scripts

--- client/features/playground/Playground.tsx
+++ client/features/playground/Playground.tsx
@@ -5,40 +5,7 @@ import { ScriptEditor } from './scriptEditor/ScriptEditor';
 import type { Block } from './types';
 
 export const Playground = () => {
-  const [scripts, setScripts] = useState<Block[][]>([
-    [
-      { id: 0, arg: [] },
-      {
-        id: 8,
-        arg: [
-          'true',
-          [
-            {
-              id: 6,
-              arg: [
-                'false',
-                [
-                  { id: 1, arg: ['10'] },
-                  { id: 1, arg: ['10'] },
-                ],
-              ],
-            },
-            {
-              id: 6,
-              arg: [
-                'true',
-                [
-                  { id: 1, arg: ['10'] },
-                  { id: 1, arg: ['10'] },
-                ],
-              ],
-            },
-          ],
-        ],
-      },
-      { id: 1, arg: ['10'] },
-    ],
-  ]);
+  const [scripts, setScripts] = useState<Block[][]>([[]]);
   return (
     <div className={styles.main}>
       <ScriptEditor scripts={scripts} setScripts={setScripts}></ScriptEditor>
