a749f60b049b8136d3a6c4b226e08017e48fc9c6
yossuli <s1f102302348@iniad.org>
2024-09-10 09:48:08 +0900

    add: dragged block shadow

--- client/features/playground/scriptEditor/scriptEditSpace/ScriptEditSpace.tsx
+++ client/features/playground/scriptEditor/scriptEditSpace/ScriptEditSpace.tsx
@@ -2,7 +2,7 @@
 import { BLOCKS_DICT } from 'features/playground/constants';
 import type { Block, BLOCK, blockArg } from 'features/playground/types';
 import type { Dispatch, SetStateAction } from 'react';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { isArg } from '../../utils/isArg';
 import styles1 from '../ScriptEditor.module.css';
 import styles from './ScriptEditSpace.module.css';
@@ -54,68 +54,100 @@ type ScriptBlockProps = {
   indexes: number[];
   handleOnChange: (e: React.ChangeEvent<HTMLInputElement>, n: number, is: number[]) => void;
   handleDrop: (e: React.DragEvent<HTMLElement>, n: number, is: number[]) => void;
+  resetParentIsDragOver?: () => void;
 };
 
 const ScriptBlock = (props: ScriptBlockProps) => {
-  const { block, scriptIndex, indexes, handleOnChange, handleDrop } = props;
+  const { block, scriptIndex, indexes, handleOnChange, handleDrop, resetParentIsDragOver } = props;
+  const [isDragOver, setIsDragOver] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const dragOverChildElement = () => {
+    setIsDragOver(false);
+  };
+
   return (
-    <div
-      style={{ display: 'flex', flexDirection: 'row' }}
-      onDrop={(e) => handleDrop(e, scriptIndex, indexes)}
-    >
-      {BLOCKS_DICT[block.id]?.contents.map((content, i, contents) => {
-        if (isArg(content)) {
-          const argIndex = contents.slice(0, i).filter(isArg).length;
-          const newIndexes = [...indexes, argIndex];
-          const arg = block.arg[argIndex];
-          if (typeof arg === 'string') {
+    <>
+      <div
+        style={{ display: 'flex', flexDirection: 'row' }}
+        onDrop={(e) => {
+          setIsDragOver(false);
+          handleDrop(e, scriptIndex, indexes);
+        }}
+        onMouseOver={(e) => e.stopPropagation()}
+        className={styles1.block}
+        ref={ref}
+        onDragOver={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          resetParentIsDragOver?.();
+          setIsDragOver(true);
+        }}
+        onDragLeave={() => setIsDragOver(false)}
+        onDragEnd={() => setIsDragOver(false)}
+      >
+        {BLOCKS_DICT[block.id]?.contents.map((content, i, contents) => {
+          if (isArg(content)) {
+            const argIndex = contents.slice(0, i).filter(isArg).length;
+            const newIndexes = [...indexes, argIndex];
+            const arg = block.arg[argIndex];
+            if (typeof arg === 'string') {
+              return (
+                <input
+                  className={styles1.input}
+                  key={i}
+                  type="text"
+                  defaultValue={arg}
+                  onChange={(e) => handleOnChange(e, scriptIndex, newIndexes)}
+                  onDrop={(e) => {
+                    setIsDragOver(false);
+                    handleDrop(e, scriptIndex, newIndexes);
+                  }}
+                />
+              );
+            }
+            if (arg instanceof Array) {
+              return (
+                <div style={{ flexDirection: 'column' }} key={i}>
+                  {arg.map((scriptBlock, j) => (
+                    <ScriptBlock
+                      key={`${i}-${j}`}
+                      block={scriptBlock}
+                      scriptIndex={scriptIndex}
+                      indexes={[...newIndexes, j]}
+                      handleOnChange={handleOnChange}
+                      handleDrop={handleDrop}
+                      resetParentIsDragOver={dragOverChildElement}
+                    />
+                  ))}
+                  <input
+                    className={styles1.input}
+                    type="text"
+                    defaultValue={''}
+                    onDrop={(e) => {
+                      setIsDragOver(false);
+                      handleDrop(e, scriptIndex, [...newIndexes, arg.length]);
+                    }}
+                  />
+                </div>
+              );
+            }
             return (
-              <input
-                className={styles1.input}
+              <ScriptBlock
                 key={i}
-                type="text"
-                defaultValue={arg}
-                onChange={(e) => handleOnChange(e, scriptIndex, newIndexes)}
-                onDrop={(e) => handleDrop(e, scriptIndex, newIndexes)}
+                block={arg}
+                scriptIndex={scriptIndex}
+                indexes={newIndexes}
+                handleOnChange={handleOnChange}
+                handleDrop={handleDrop}
+                resetParentIsDragOver={dragOverChildElement}
               />
             );
           }
-          if (arg instanceof Array) {
-            return (
-              <div style={{ flexDirection: 'column' }} key={i}>
-                {arg.map((scriptBlock, j) => (
-                  <ScriptBlock
-                    key={`${i}-${j}`}
-                    block={scriptBlock}
-                    scriptIndex={scriptIndex}
-                    indexes={[...newIndexes, j]}
-                    handleOnChange={handleOnChange}
-                    handleDrop={handleDrop}
-                  />
-                ))}
-                <input
-                  className={styles1.input}
-                  type="text"
-                  defaultValue={''}
-                  onDrop={(e) => handleDrop(e, scriptIndex, [...newIndexes, arg.length])}
-                />
-              </div>
-            );
-          }
-          return (
-            <ScriptBlock
-              key={i}
-              block={arg}
-              scriptIndex={scriptIndex}
-              indexes={newIndexes}
-              handleOnChange={handleOnChange}
-              handleDrop={handleDrop}
-            />
-          );
-        }
-        return <div key={i}>{content}</div>;
-      })}
-    </div>
+          return <div key={i}>{content}</div>;
+        })}
+      </div>
+      {isDragOver && <div className={`${styles1.block} ${styles.blockGhost}`}></div>}
+    </>
   );
 };
 
@@ -178,16 +210,14 @@ export const ScriptEditSpace = (scriptEditSpaceProps: Props) => {
       {scripts.map((script, scriptIndex) => (
         <div key={scriptIndex}>
           {script.map((block, n) => (
-            <div className={styles1.block} key={n}>
-              <ScriptBlock
-                key={`${scriptIndex}-${n}`}
-                block={block}
-                scriptIndex={scriptIndex}
-                indexes={[n]}
-                handleOnChange={handleOnChange}
-                handleDrop={handleDropToInput}
-              />
-            </div>
+            <ScriptBlock
+              key={`${scriptIndex}-${n}`}
+              block={block}
+              scriptIndex={scriptIndex}
+              indexes={[n]}
+              handleOnChange={handleOnChange}
+              handleDrop={handleDropToInput}
+            />
           ))}
           {script && (
             <>
