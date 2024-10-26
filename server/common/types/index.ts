a0535b2dabefe4d3d5b670baeacfdf96234ea032
yossuli <s1f102302348@iniad.org>
2024-10-27 04:37:25 +0900

    add: type SubKeyObj

--- server/common/types/index.ts
+++ server/common/types/index.ts
@@ -3,3 +3,5 @@ export type StrictOmit<T, U extends keyof T> = Pick<T, Exclude<keyof T, U>>;
 export type NonNullableObj<T> = {
   [K in keyof T]: NonNullable<T[K]>;
 };
+
+export type SubKeyObj<T, U extends { [K in keyof T]?: unknown }> = U;
