fb3ca9bbd5211bf97ee08becbb33da825e552f43
yossuli <s1f102302348@iniad.org>
2024-10-27 21:31:59 +0900

    fix: phraseUpdateVal

--- server/common/types/phrase.ts
+++ server/common/types/phrase.ts
@@ -1,4 +1,4 @@
-import type { StrictOmit, SubKeyObj } from '.';
+import type { StrictOmit } from '.';
 import type { DtoId, MaybeId } from './brandedId';
 
 export type PhraseDto = {
@@ -12,10 +12,6 @@ export type PhraseCreateVal = StrictOmit<PhraseDto, 'id'>;
 
 export type PhraseUpdateOrderVal = { phraseId: MaybeId['phrase']; order: number };
 
-export type PhraseUpdateVal = StrictOmit<PhraseDto, 'id' | 'phraseGroupId'> &
-  SubKeyObj<
-    PhraseDto,
-    {
-      id: MaybeId['phrase'];
-    }
-  >;
+export type PhraseUpdateDto = StrictOmit<PhraseDto, 'id' | 'phraseGroupId'> & {
+  phraseId: MaybeId['phrase'];
+};
