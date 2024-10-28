0c5f4215939042dcc511cd9ad84867f63d193a03
yossuli <s1f102302348@iniad.org>
2024-10-28 09:41:07 +0900

    add: phrase find flow

--- server/domain/phrase/repository/phraseQuery.ts
+++ server/domain/phrase/repository/phraseQuery.ts
@@ -1,5 +1,6 @@
 import type { Phrase, Prisma } from '@prisma/client';
 import { brandedId } from 'service/brandedId';
+import { depend } from 'velona';
 import type { PhraseEntity } from '../model/phraseType';
 
 const toPhraseEntity = (prismaPhrase: Phrase): PhraseEntity => ({
@@ -9,11 +10,28 @@ const toPhraseEntity = (prismaPhrase: Phrase): PhraseEntity => ({
   phraseGroupId: brandedId.phraseGroup.entity.parse(prismaPhrase.phraseGroupId),
 });
 
+const listByGroupId = async (
+  tx: Prisma.TransactionClient,
+  phraseGroupId: string,
+): Promise<PhraseEntity[]> => {
+  const phrases = await tx.phrase.findMany({
+    where: { phraseGroupId },
+    orderBy: { indexInGroup: 'asc' },
+  });
+  return phrases.map(toPhraseEntity);
+};
+
 export const phraseQuery = {
+  listByGroupId,
   findById: async (tx: Prisma.TransactionClient, phraseId: string): Promise<PhraseEntity> => {
     const phrase = await tx.phrase.findUniqueOrThrow({
       where: { id: phraseId },
     });
     return toPhraseEntity(phrase);
   },
+  listByGroupIdWithDI: depend(
+    { listByGroupId },
+    (deps, tx: Prisma.TransactionClient, phraseGroupId: string): Promise<PhraseEntity[]> =>
+      deps.listByGroupId(tx, phraseGroupId),
+  ),
 };
