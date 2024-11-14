6529dc80f2c5ce71553f3234b681c57a06472b96
yossuli <s1f102302348@iniad.org>
2024-11-14 15:43:58 +0900

    fix: import file name

--- client/features/playground/preview/ControlBar/ControlBar.tsx
+++ client/features/playground/preview/ControlBar/ControlBar.tsx
@@ -1,4 +1,4 @@
-import styles from './Controls.module.css';
+import styles from './ControlBar.module.css';
 
 type Props = {
   isActive: boolean;
--- client/features/playground/preview/Obstacles/Obstacles.tsx
+++ client/features/playground/preview/Obstacles/Obstacles.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Obstacle.module.css';
+import styles from './Obstacles.module.css';
 
 type Props = Partial<Pick<React.CSSProperties, 'backgroundColor' | 'border'>> & {
   backGroundImgURL?: string;
